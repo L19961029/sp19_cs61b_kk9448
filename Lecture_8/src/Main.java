97af5ded2068bdb830c7ce9f14b9aa5d308ac634
正义碳 <1633100573@qq.com>
2020-04-26 23:26:07 -0400

    Update Main.java

--- Lecture_8/src/Main.java
+++ Lecture_8/src/Main.java
@@ -1,2 +1,46 @@
 public class Main {
+    public static void main(String[] args) {
+        dogA a=new dogA();
+        dogB b=new dogB();
+        make_dog_shout.make_dog_shout(a);
+        make_dog_shout.make_dog_shout(b);
+        dog c=new dogA();
+        c.makeNoise();
+        dogA d=(dogA) c;
+        System.out.println(d.b);
+    }
 }
+class make_dog_shout{
+    static void make_dog_shout(dog A){
+        A.makeNoise();
+    }
+//   static void make_dog_shout(dogA x){
+//       dogA.makeNoise();
+//    }
+//    static void make_dog_shout(dogB x){
+//        dogB.makeNoise();
+//    }
+}
+interface dog{
+    void makeNoise();
+
+}
+class dogA implements dog{
+    int b=99;
+    @Override
+    public void makeNoise(){
+        b=100;
+        System.out.println("i am dog a");
+    }
+    public void makeNoise2(){
+//         b=100;
+        System.out.println("not implement");
+    }
+}
+class dogB implements dog{
+    @Override
+    public void makeNoise(){
+        System.out.println("i am dog b");
+    }
+}
+
