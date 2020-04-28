121bb351cc962b41cc8defef5e32112e8faf2918
正义碳 <1633100573@qq.com>
2020-04-28 18:06:00 -0400

    Lab 3 fix

--- lab3/Flik/FlikTest.java
+++ lab3/Flik/FlikTest.java
@@ -6,6 +6,11 @@ public class FlikTest {
 
     @Test
     public void test(){
+        Integer a = 500;
+        Integer b = 500;
+//        assertTrue("errr", Flik.isSameNumber(a,b));
+        System.out.println(Flik.isSameNumber(a,b));
+
 
     }
 
